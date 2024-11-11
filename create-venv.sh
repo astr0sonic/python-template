c84e9f8b65f108d27df1b9e6d5d96e593ad76240
astrosonic <z.astrosonic@gmail.com>
2024-11-11 17:52:05 +0300

    install pre-commit

--- create-venv.sh
+++ create-venv.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
-python3.11 -m venv .venv
+python3.11 -m venv --clear .venv
 source .venv/bin/activate
 pip install -r requirements.txt
+pip install -r requirements-dev.txt
+pre-commit install
 deactivate
